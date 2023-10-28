8af8b7310c58fec773ff99b881eda726411fc190
Leo-Youmbi <leoyoumbi83@gmail.com>
2023-10-28 17:03:14 +0100

    feat: doing task 3

---
+++ api/v1/views/index.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module for route /status"""
+from api.v1.views import app_views
+from flask import jsonify
+import models
+
+@app_views.route('/status', methods=['GET'])
+def status_view():
+    """Returns Successful status"""
+    return jsonify({'status': 'OK'})
