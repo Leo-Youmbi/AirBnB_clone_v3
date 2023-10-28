42591e962d1f2362474e0a7d2fc53f2985beaf47
Leo-Youmbi <leoyoumbi83@gmail.com>
2023-10-28 17:33:49 +0100

    fix: aligning to pycodestyle

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,10 +4,12 @@ from api.v1.views import app_views
 from flask import jsonify
 from models import storage
 
+
 @app_views.route('/status', methods=['GET'])
 def status_view():
     """Returns Successful status"""
     return jsonify({'status': 'OK'})
+
 
 @app_views.route('/stats', methods=['GET'])
 def stats_view():
